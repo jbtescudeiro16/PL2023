fce52fd7748129de2553ca12c82a7b7b0f8a4aa1
Bernardo Escudeiro <jbtescudeiro@gmail.com>
2023-02-24 10:16:24 +0000

    Homework2-Done

--- TPC2/main.py
+++ TPC2/main.py
@@ -2,13 +2,14 @@
 
 
 def parsercommandline():
+    ligado=1
     soma = 0;
     k=input()
     while k!='':
         text = k
         i = 0
         while i < len(text):
-            if text[i].isdigit():
+            if text[i].isdigit() and ligado==1:
                 seq = text[i]
                 j = i + 1
                 while j < len(text) and text[j].isdigit():
@@ -19,17 +20,29 @@ def parsercommandline():
             else:
                 if text[i] =="=":
                     print("A soma está em: "+str(soma))
+                elif (text[i] == "O" or text[i] == "o"):
+                    if i + 1 < len(text):
+                        j = i + 1
+                        if (text[j] == "f" or text[j] == "F"):
+                            if (i + 2 < len(text)):
+                                k = i + 2
+                                if (text[k] == "f" or text[k] == "F"):
+                                    ligado=0
+                        elif (text[j] == "n" or text[j] == "N"):
+                                    ligado=1
+
                 i += 1
         k=input()
 
-    print( soma)
+    print("O resultado final é: " +str(soma))
 def parser(filepath):
-    soma=0;
+    ligado=1
+    soma=0
     with open(filepath, 'r') as file:
         text = file.read()
         i = 0
         while i < len(text):
-            if text[i].isdigit():
+            if text[i].isdigit() and ligado==1:
                 seq = text[i]
                 j = i + 1
                 while j < len(text) and text[j].isdigit():
@@ -40,14 +53,27 @@ def parser(filepath):
             else:
                 if text[i] =="=":
                     print("A soma está em: "+str(soma))
+                elif (text[i] == "O" or text[i] == "o"):
+                    if i + 1 < len(text):
+                        j = i + 1
+                        if (text[j] == "f" or text[j] == "F"):
+                            if (i + 2 < len(text)):
+                                k = i + 2
+                                if (text[k] == "f" or text[k] == "F"):
+                                    ligado=0
+                        elif (text[j] == "n" or text[j] == "N"):
+                                    ligado=1
                 i += 1
 
-        print( "A soma dos digitos no ficheiro é:"+str(soma))
+        print("O resultado final é: " +str(soma))
+
+
+
 if __name__ == '__main__':
     print("------------PL2023-TPC2-A96075------------")
     print("|Insira de onde pretende obter o Input : |")
     print("|1-> Ficheiro                            |")
-    print("|2->Linha de Comandos                    |")
+    print("|2-> Linha de Comandos                    |")
     print("------------------------------------------")
     opt=input()
     if opt=='1':
